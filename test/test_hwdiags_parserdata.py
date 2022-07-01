bf086e6031dfc8d723f68adab638eea9f006c511
Zane Shelley <zshelle@us.ibm.com>
2022-07-01 09:38:59 -0500

    Update unit test instructions
    The current instructions in the README.md requires that each test case
    adds the `modules` directory to the import path. This can be avoided if
    the unit test command is run from the `modules` directory.

--- test/test_hwdiags_parserdata.py
+++ test/test_hwdiags_parserdata.py
@@ -1,7 +1,3 @@
-# Add the modules directory to the path.
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'modules'))
-
 import unittest
 
 from collections import OrderedDict
@@ -53,14 +49,14 @@ class TestParserData(unittest.TestCase):
         self.assertEqual(data, exp_chip)
 
     def test_sig_data(self):
-        sig_id   = '5555'
+        sig_id = '5555'
         sig_inst = 0x66
-        sig_bit  = 0x77
+        sig_bit = 0x77
 
         data = self._parser.get_sig_desc(self._model_ec, sig_id, sig_inst,
                                          sig_bit)
 
-        exp_sig  = "id:%s(%d)[%s] " % (sig_id.upper(), sig_inst, sig_bit)
+        exp_sig = "id:%s(%d)[%s] " % (sig_id.upper(), sig_inst, sig_bit)
 
         self.assertEqual(data, exp_sig)
 
@@ -73,18 +69,18 @@ class TestParserData(unittest.TestCase):
 
         model_ec = word_a
 
-        chip_pos  = int(word_b[0:4], base=16)
-        node_pos  = int(word_b[4:6], base=16)
+        chip_pos = int(word_b[0:4], base=16)
+        node_pos = int(word_b[4:6], base=16)
         attn_type = int(word_b[6:8], base=16)
 
-        sig_id   =     word_c[0:4]
+        sig_id = word_c[0:4]
         sig_inst = int(word_c[4:6], base=16)
-        sig_bit  = int(word_c[6:8], base=16)
+        sig_bit = int(word_c[6:8], base=16)
 
         exp_chip = "node %d unknown %d (%s)" % (node_pos, chip_pos,
                                                 model_ec.upper())
 
-        exp_sig  = "id:%s(%d)[%s] " % (sig_id.upper(), sig_inst, sig_bit)
+        exp_sig = "id:%s(%d)[%s] " % (sig_id.upper(), sig_inst, sig_bit)
 
         exp_attn = str(attn_type)
 
@@ -94,4 +90,3 @@ class TestParserData(unittest.TestCase):
         expected["Attn Type"] = exp_attn
 
         self.assertEqual(data, expected)
-
