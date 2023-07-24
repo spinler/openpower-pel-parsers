07a7e63013d90d1f327cb142a6db2b0fec1cd62a
Matt Spinler <spinler@us.ibm.com>
2023-07-24 13:03:58 -0500

    peltool: Add Config class (#29)
    This class holds configuration options for peltool.
    
    It should have been part of the previous commit but was missed.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ modules/pel/peltool/config.py
@@ -0,0 +1,9 @@
+class Config:
+    """
+    Holds configuration options.
+    """
+
+    def __init__(self) -> None:
+        self.allow_plugins = True
+        self.serviceable_only = False
+        self.non_serviceable_only = False
