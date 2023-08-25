983cfed147a3935c628f38d9d97a9123a4349c34
Matt Spinler <spinler@us.ibm.com>
2023-08-25 08:43:10 -0500

    peltool-wrapper: Add hcode-pel-parser
    There is a new peltool module, so include it here.
    
    Also bump this package's version, and format with black.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- peltool-wrapper/setup.py
+++ peltool-wrapper/setup.py
@@ -4,19 +4,20 @@ import os
 # This package is just used to install all of the component peltool
 # packages and doesn't contain any code of its own.
 
-version = os.getenv('PELTOOL_VERSION', '0.2')
+version = os.getenv("PELTOOL_VERSION", "0.3")
 
 setup(
-    name        = "peltool-wrapper",
-    version     = f'{version}',
-    classifiers = [ "License :: OSI Approved :: Apache Software License" ],
-    packages = ['peltool-wrapper'],
-    install_requires = [
+    name="peltool-wrapper",
+    version=f"{version}",
+    classifiers=["License :: OSI Approved :: Apache Software License"],
+    packages=["peltool-wrapper"],
+    install_requires=[
         f"openpower-pel-parsers >= {version}",
         f"Hostboot >= {version}",
         f"sbe-pel-parser >= {version}",
         f"pel-message-registry >= {version}",
         f"openpower-hw-diags-pel-parser-data >= {version}",
-        f"occ-pel-parser >= {version}"
-        ]
+        f"occ-pel-parser >= {version}",
+        f"hcode-pel-parser >= {version}"
+    ],
 )
